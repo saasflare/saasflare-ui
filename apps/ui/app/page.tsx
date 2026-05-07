41cb6b6af914938c7f23fdb0cb27a4e1f91027de
Dr. Chris <hi@saasflare.io>
2026-05-07 15:29:01 +0200

    chore: release @saasflare/ui 3.1.1

--- apps/ui/app/page.tsx
+++ apps/ui/app/page.tsx
@@ -17,13 +17,14 @@ export default function Page() {
     <main className={'flex h-[5000] min-h-screen flex-col items-center gap-y-5 p-24'}>
       <h1>Saasflare UI</h1>
       <p>Component library demo</p>
-        <Button variant="ghost" onClick={() => console.log("test")}>Button</Button>
+        <Button variant="ghost" intent="neutral" onClick={() => console.log("test")}>Button</Button>
         <Button variant="link">Button</Button>
         <Button variant="soft">Button</Button>
         <Button variant="solid">Button</Button>
-        <Button variant="shadow" className="invert">Button</Button>
-        <Button variant="outline">Button</Button>
+        <Button variant="shadow">Button</Button>
+        <Button variant="outline" intent="neutral">Button</Button>
         <Button variant="outline" fullWidth loading={loading}>Button</Button>
+        <span className="text-secondary">Test</span>
       <UserAvatar src={"https://media.licdn.com/dms/image/v2/D4E03AQGEs8OYEo0j5g/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1726815764755?e=1779926400&v=beta&t=jr6oR9MlZ0h1WRbwP00i2hngT1s888ypaz-CMOLoovs"} name={"Dr. Chris"} initials={"CM"}/>
     </main>
   );
